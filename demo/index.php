966025ff60ff260e2ef63999629c735c99e449dc
Abdul Samad <a.samad@digitonix.pk>
2018-11-30 15:37:59 +0500

    asdsad

--- demo/index.php
+++ demo/index.php
@@ -1,5 +1,5 @@
 <?php 
-$endpoint = "https://".$_SERVER['HTTP_HOST']."/demo/server/server.php/";
+$endpoint = "http://".$_SERVER['HTTP_HOST']."/demo/server/files/";
 
 ?>
 <!doctype html>
--- demo/server/server.php
+++ demo/server/server.php
@@ -3,7 +3,7 @@ error_reporting(1);
 require 'vendor/autoload.php';
 
 $server   = new \TusPhp\Tus\Server('');
-$server->setApiPath('/server.php'); // tus server endpoint.
+$server->setApiPath('/files'); // tus server endpoint.
 //$server->setUploadDir('/tmp'); // uploads dir.
 $response = $server->serve();
 $response->send();
