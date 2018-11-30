e06946309730e471c3d7bf6064160ed42347fa24
Abdul Samad <a.samad@digitonix.pk>
2018-11-30 15:22:22 +0500

    asdsad

--- demo/server/server.php
+++ demo/server/server.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(1);
 require 'vendor/autoload.php';
-
+die("in ser");
 $server   = new \TusPhp\Tus\Server('');
-$server->setApiPath('/server.php'); // tus server endpoint.
+$server->setApiPath('/server/server.php'); // tus server endpoint.
 //$server->setUploadDir('/tmp'); // uploads dir.
 $response = $server->serve();
 $response->send();
